c68f6e627a8cd01f6dec7018cbd289709c00b7a6
clairebianchi <41926844+clairebianchi@users.noreply.github.com>
2018-09-13 10:50:20 -0700

    Fix bug in script
    The flag for duration was incorrect, should be -d, was -f so the script was not creating a scratch org

--- scripts/orgInit.sh
+++ scripts/orgInit.sh
@@ -6,7 +6,7 @@ if [ "$#" -eq 1 ]; then
   DURATION=$1
 fi
 
-sfdx force:org:create -a dreamhouse -s -f config/project-scratch-def.json -f $DURATION
+sfdx force:org:create -a dreamhouse -s -f config/project-scratch-def.json -d $DURATION
 sfdx force:source:push
 sfdx force:user:permset:assign -n dreamhouse
 sfdx force:org:open -p /lightning/page/home
