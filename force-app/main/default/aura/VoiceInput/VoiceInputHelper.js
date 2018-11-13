e6dfb84602b2093e074343ecf2dbcfd40ca9fda9
Christophe Coenraets <ccoenraets@gmail.com>
2018-11-13 11:05:06 -0500

    fix VoinceInputHelper text visibility

--- force-app/main/default/aura/VoiceInput/VoiceInputHelper.js
+++ force-app/main/default/aura/VoiceInput/VoiceInputHelper.js
@@ -3,9 +3,5 @@
         var voiceInputChange = component.getEvent("onchange");
         voiceInputChange.setParams({value: component.get("v.utterance")});
         voiceInputChange.fire();
-        component.set("v.utterance", "");
-        var utteranceEl = component.find("utterance").getElement();
-        utteranceEl.value = "";
-        utteranceEl.placeholder = "";
     }
 })
